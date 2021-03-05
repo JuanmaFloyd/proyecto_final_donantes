fd9bc0915371486025a36a0d5a1944d855223ab6
Stefano Canossini <31194186+scanossini@users.noreply.github.com>
2021-03-05 20:43:15 -0300

    asd

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -14,22 +14,69 @@ const useStyles = makeStyles({
 export const Perfil = () => {
     const classes = useStyles();
     const [donante, setDonante] = useState("")
-
+    
     useEffect(() => {
         axios.get("http://localhost:5000/donante/info", {"headers": {"token": sessionStorage.getItem("dtoken")}})
             .then(response => setDonante(response.data))
     }, [])
 
+    const fechaNac = () => {
+        return new Date(donante.fechaNac)
+    }
+
+    function parseDate() {
+        if(donante != null){
+            return fechaNac().getDay()+"/"+(fechaNac().getMonth()+1)+"/"+fechaNac().getFullYear()
+        }
+        
+    }
+
     return(
         donante ?
         <Container>
-            {JSON.stringify(donante)}
             <form>
                 <label>
                     Nombre y apellido: {donante.nombre} {donante.apellido}
                 </label>
+                <div></div>
                 <label>
                     DNI: {donante.dni}
+                </label>
+                <div></div>
+                <label>
+                    Tipo de sangre: {donante.tipoDeSangre}
+                </label>
+                <div></div>
+                <label>
+                    Fecha de nacimiento: {donante.fechaNac} {parseDate()}
+                </label>
+                <div></div>
+                <label>
+                    Sexo: {donante.sexo}
+                </label>
+                <div></div>
+                <label>
+                    Domicilio {donante.domicilio}
+                </label>
+                <div></div>
+                <label>
+                    Localidad: {donante.localidad}
+                </label>
+                <div></div>
+                <label>
+                    Telefono: {donante.telefono}
+                </label>
+                <div></div>
+                <label>
+                    Mail: {donante.mail}
+                </label>
+                <div></div>
+                <label>
+                    Ocupación: {donante.ocupacion}
+                </label>
+                <div></div>
+                <label>
+                    Última Donación: {donante.fechaDonacion}
                 </label>
             </form>
 
