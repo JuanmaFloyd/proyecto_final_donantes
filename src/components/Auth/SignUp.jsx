6aa77828fc98ce7ae3c27dbdfa4dcc2c22a98349
Stefano Canossini <31194186+scanossini@users.noreply.github.com>
2021-03-31 21:56:37 -0300

    fix warnings

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -1,5 +1,4 @@
 import { Button, Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@material-ui/core'
-import { KeyboardReturnOutlined } from '@material-ui/icons';
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useState } from 'react';
--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import * as AiIcons from "react-icons/ai";
+import { AiFillHome, AiFillDatabase, AiFillSmile, AiOutlineLogout } from "react-icons/ai";
 
 export const SidebarData = [
     {
         title: 'Inicio',
         path: '/',
-        icon: <AiIcons.AiFillHome />,
+        icon: <AiFillHome />,
         cName: 'nav-text'
     },
     {
         title: 'Solicitudes',
         path: '/solicitudes',
-        icon: <AiIcons.AiFillDatabase />,
+        icon: <AiFillDatabase />,
         cName: 'nav-text'
     },
     {
         title: 'Mis datos',
         path: '/perfil',
-        icon: <AiIcons.AiFillSmile />,
+        icon: <AiFillSmile />,
         cName: 'nav-text'
     },
     {
         title: 'Cerrar Sesión',
         path: '/logout',
-        icon: <AiIcons.AiOutlineLogout />,
+        icon: <AiOutlineLogout />,
         cName: 'nav-text'
     }
 ] 
