ff2baf7c630f46dcb72071ad4e69714148fea871
Juan Martin Giron <jgiron@hexacta.com>
2021-04-08 13:18:01 -0300

    edit donante (no terminado)

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,9 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Box, Grid, Card, CardContent, Container, Divider, Typography } from '@material-ui/core'
+import { Box, Grid, Card, CardContent, Container, Divider, Typography, Fab, makeStyles, TextField } from '@material-ui/core'
+import EditIcon from "@material-ui/icons/Edit";
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles({
+    button: {
+        position: "fixed",
+        right: "100px",
+        bottom: "40px",
+    },
+    button2: {
+        position: "fixed",
+        right: "180px",
+        bottom: "40px",
+    },
+    animated: {
+        animationName: '$fadein',
+        animationDuration: '0.5s',
+        animationFillMode: 'forwards'
+      },
+      '@keyframes fadein': {
+        '0%': {
+            position: "fixed",
+            right: "100px",
+            bottom: "40px",
+        },
+        '100%': {
+            position: "fixed",
+            right: "180px",
+            bottom: "40px",
+        }
+      }
+})
 
 export const Perfil = () => {
     const [donante, setDonante] = useState("")
+    const [edit, setEdit] = useState(false)
+    const [domicilio, setDomicilio] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [localidad, setLocalidad] = useState("");
+    const [ocupacion, setOcupacion] = useState("");
+    const [mail, setMail] = useState("");
+    const classes = useStyles()
     
     useEffect(() => {
         axios.get("http://localhost:5000/donante/info", {"headers": {"token": sessionStorage.getItem("dtoken")}})
@@ -20,9 +60,20 @@ export const Perfil = () => {
         var ultimaDon = new Date(donante.fechaDonacion)
         return Math.floor((hoy.getTime() - ultimaDon.getTime()) / (1000 * 3600 * 24))
     }
+    
+    const handleEdit = () => {
+        var data = {
+            localidad: localidad,
+            domicilio: domicilio,
+            telefono: telefono,
+            mail: mail,
+            ocupacion: ocupacion
+        }
+    }
 
     return(
         donante ?
+        !edit ?
         <Container>
             <Grid style={{ padding: 10 }} container spacing={3}>
                 <Grid item xs={12}>
@@ -48,7 +99,7 @@ export const Perfil = () => {
                     <Card>
                         <CardContent>
                             Tipo de sangre: {donante.tipoDeSangre}
-                            </CardContent>
+                        </CardContent>
                     </Card>
                 </Grid>
                 <Grid item xs={4}>
@@ -110,7 +161,88 @@ export const Perfil = () => {
                     </Card>
                 </Grid>
             </Grid>
-        </Container>
+            <Fab onClick={() => setEdit(true)} className={classes.button} color="primary">
+                <EditIcon />
+            </Fab>
+        </Container> 
+        : 
+        <Container>
+            <Grid style={{ padding: 10 }} container spacing={3}>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardContent>
+                            <Typography gutterBottom={true} align={'center'} variant="h4">{donante.nombre} {donante.apellido}</Typography>
+                        </CardContent>
+                    </Card>
+                    <Grid item xs={12}>
+                        <Box align={'center'} m={2}>
+                            <Divider variant="middle" />
+                        </Box>
+                    </Grid>
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField 
+                        id="domicilio"
+                        name="domicilio"
+                        label="Domicilio"
+                        variant="outlined"
+                        defaultValue={donante.domicilio}
+                        fullWidth
+                        onChange={(event) => setDomicilio(event.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField 
+                        id="localidad"
+                        name="localidad"
+                        label="Localidad"
+                        variant="outlined"
+                        defaultValue={donante.localidad}
+                        fullWidth
+                        onChange={(event) => setLocalidad(event.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField 
+                        id="ocupacion"
+                        name="ocupacion"
+                        label="Ocupación"
+                        variant="outlined"
+                        defaultValue={donante.ocupacion}
+                        fullWidth
+                        onChange={(event) => setOcupacion(event.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField 
+                        id="mail"
+                        name="mail"
+                        label="E-mail"
+                        variant="outlined"
+                        defaultValue={donante.mail}
+                        fullWidth
+                        onChange={(event) => setMail(event.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField 
+                        id="telefono"
+                        name="telefono"
+                        label="Teléfono"
+                        variant="outlined"
+                        defaultValue={donante.telefono}
+                        fullWidth
+                        onChange={(event) => setTelefono(event.target.value)}
+                    />
+                </Grid>
+            </Grid>
+            <Fab onClick={() => setEdit(false)} className={classes.animated} color="secondary">
+                <CloseIcon />
+            </Fab>
+            <Fab onClick={() => setEdit(false)} className={classes.button} color="primary">
+                <CheckIcon />
+            </Fab>
+        </Container> 
         : null
     )
 }
